84f1da288915c009597e936ab2bc10b4d060f938
Cynthia Castillo <ca.castillo@gmail.com>
2022-05-03 17:50:58 -0500

    Ruteo

---
+++ src/Componentes/Home/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div className='container'>
+            <h1>¡Bienvenidos!</h1>
+        </div>
+    )
+}
+
+export default Home
